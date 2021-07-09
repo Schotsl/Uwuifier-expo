9e3ec6cd7322189a84f7dd620aafbf699aab366e
Schotsl <sjorsvanholst@gmail.com>
2021-07-09 22:01:41 +0200

    🎨 Formatted codebase

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const uwuifier = new Uwuifier();
 
 export default function App() {
   const [input, setText] = useState(
-    "Hey! This site can help you make any old boring text nice and uwu. We can't imagine anyone would actually use this, but you gotta do what you gotta do."
+    "Hey! This site can help you make any old boring text nice and uwu. We can't imagine anyone would actually use this, but you gotta do what you gotta do.",
   );
 
   return (
