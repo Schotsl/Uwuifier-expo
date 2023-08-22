7fb732fd93b7ff52dd51d64b43a0556dff0ba066
Sjors van Holst <sjorsvanholst@gmail.com>
2023-08-22 21:50:10 +0200

    ♻️ Switched to latest Expo version

--- App.tsx
+++ App.tsx
@@ -1,118 +1,20 @@
-import Uwuifier from "uwuifier";
-
-import { Label } from "./components/Label.tsx";
-import { Header } from "./components/Header.tsx";
-import { Inputs } from "./components/Inputs.tsx";
-
-import React, { useState } from "react";
-
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-
-const uwuifier = new Uwuifier();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [changed, setChanged] = useState(false);
-  const [next, setNext] = useState(0);
-  const [input, setText] = useState(
-    "Hey! This site can help you make any old boring text nice and uwu. We can't imagine anyone would actually use this, but you gotta do what you gotta do.",
-  );
-
-  function onChange(content: string) {
-    setText(content);
-
-    if (changed) {
-      const timeout = setTimeout(addHistory, 1000);
-
-      setNext(timeout);
-      clearTimeout(next);
-    }
-
-    setChanged(true);
-  }
-
-  function addHistory() {
-    const body = JSON.stringify({ origin: "android" });
-    const method = "POST";
-    const headers = { "Content-Type": "application/json" };
-
-    fetch("https://api.uwuifier.com/v1/history", { method, body, headers });
-  }
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.body}>
-        <Header />
-
-        <View style={styles.inputs}>
-          <Label
-            color="#fff"
-            background="#252525"
-            label="Input"
-            radius={true}
-          />
-
-          <View style={[styles.input, { backgroundColor: "#252525" }]}>
-            <TextInput
-              style={[styles.input, { color: "#ffffff" }]}
-              placeholder="Type something"
-              placeholderTextColor="grey"
-              onChangeText={onChange}
-              multiline={true}
-              value={input}
-            />
-          </View>
-
-          <Label
-            color="#252525"
-            background="#ffc83d"
-            label="Output"
-            radius={false}
-          />
-
-          <View
-            style={[
-              styles.input,
-              {
-                backgroundColor: "#ffc83d",
-                borderBottomEndRadius: 6,
-                borderBottomStartRadius: 6,
-              },
-            ]}
-          >
-            <ScrollView>
-              <Text style={[styles.input, { color: "#000000" }]}>
-                {uwuifier.uwuifySentence(input)}
-              </Text>
-            </ScrollView>
-
-            <Inputs uwuified={uwuifier.uwuifySentence(input)} />
-          </View>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  body: {
-    height: "100%",
-    display: "flex",
-    backgroundColor: "#303030",
-  },
-  inputs: {
+  container: {
     flex: 1,
-    margin: 20,
-    shadowColor: "black",
-    shadowOffset: { width: 6, height: 6 },
-    shadowRadius: 6,
-    shadowOpacity: 0.25,
-  },
-  input: {
-    flex: 1,
-    height: "100%",
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    textAlignVertical: "top",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- metro.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-  resolver: {
-    sourceExts: ['jsx','js','ts','tsx', 'cjs'],
-   },
-};
