2c22283f98e9b7ad9f4e2614ca01f6cde2cdba4c
Sjors van Holst <sjorsvanholst@gmail.com>
2023-08-22 22:19:46 +0200

    ✨ Re-added old codebase

--- App.tsx
+++ App.tsx
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Uwuifier from "./uwuifier";
+
+import { Label } from "./components/Label";
+import { Header } from "./components/Header";
+import { Inputs } from "./components/Inputs";
+
+import React, { useState } from "react";
+
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const uwuifier = new Uwuifier();
+  const [input, setText] = useState(
+    "Hey! This site can help you make any old boring text nice and uwu. We can't imagine anyone would actually use this, but you gotta do what you gotta do.",
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.body}>
+        <Header />
+
+        <View style={styles.inputs}>
+          <Label
+            color="#fff"
+            background="#252525"
+            label="Input"
+            radius={true}
+          />
+
+          <View style={[styles.input, { backgroundColor: "#252525" }]}>
+            <TextInput
+              style={[styles.input, { color: "#ffffff" }]}
+              placeholder="Type something"
+              placeholderTextColor="grey"
+              onChangeText={(text) => setText(text)}
+              multiline={true}
+              value={input}
+            />
+          </View>
+
+          <Label
+            color="#252525"
+            background="#ffc83d"
+            label="Output"
+            radius={false}
+          />
+
+          <View
+            style={[
+              styles.input,
+              {
+                backgroundColor: "#ffc83d",
+                borderBottomEndRadius: 6,
+                borderBottomStartRadius: 6,
+              },
+            ]}
+          >
+            <ScrollView>
+              <Text style={[styles.input, { color: "#000000" }]}>
+                {uwuifier.uwuifySentence(input)}
+              </Text>
+            </ScrollView>
+
+            <Inputs uwuified={uwuifier.uwuifySentence(input)} />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
+    height: "100%",
+    display: "flex",
+    backgroundColor: "#303030",
+  },
+  inputs: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20,
+    shadowColor: "black",
+    shadowOffset: { width: 6, height: 6 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  input: {
+    flex: 1,
+    height: "100%",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    textAlignVertical: "top",
   },
 });
---
+++ components/Inputs.tsx
@@ -0,0 +1,107 @@
+import * as Clipboard from "expo-clipboard";
+import React from "react";
+
+import Svg, { Path } from "react-native-svg";
+
+import {
+  Pressable,
+  Share,
+  StyleSheet,
+  Text,
+  ToastAndroid,
+  View,
+} from "react-native";
+
+const copyText = (message: string) => {
+  Clipboard.setString(message);
+  ToastAndroid.show("Copied to clipboard", ToastAndroid.SHORT);
+};
+
+const shareText = (message: string) => {
+  Share.share({ message });
+};
+
+interface Props {
+  uwuified: string;
+}
+
+export const Inputs = (props: Props) => {
+  return (
+    <View style={styles.buttons}>
+      <Pressable
+        style={[styles.button, { marginRight: 10 }]}
+        onPress={() => shareText(props.uwuified)}
+      >
+        <Svg
+          style={[styles.svg, { marginLeft: 14, marginRight: 12 }]}
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+        >
+          <Path
+            fill="white"
+            fillRule="evenodd"
+            d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"
+          />
+        </Svg>
+      </Pressable>
+
+      <Pressable
+        style={[styles.button, { flex: 1 }]}
+        onPress={() => copyText(props.uwuified)}
+      >
+        <Svg style={[styles.svg]} width="18" height="18" viewBox="0 0 24 24">
+          <Path
+            fill="white"
+            fillRule="evenodd"
+            d="M22 6v16h-16v-16h16zm2-2h-20v20h20v-20zm-24 17v-21h21v2h-19v19h-2z"
+          />
+        </Svg>
+        <View style={[styles.divider]}></View>
+        <Text style={[styles.text]}>Copy to clipboard</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttons: {
+    height: 45,
+    display: "flex",
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+
+    borderRadius: 5,
+    backgroundColor: "#252525",
+
+    shadowColor: "black",
+    shadowOffset: { width: 4, height: 4 },
+    shadowRadius: 4,
+    shadowOpacity: 0.2,
+  },
+  text: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+
+    color: "#ffffff",
+    fontSize: 16,
+    textAlignVertical: "center",
+  },
+  divider: {
+    width: 1,
+    margin: 10,
+    height: 18,
+
+    marginLeft: 0,
+    backgroundColor: "#fff",
+  },
+  svg: {
+    margin: 13,
+  },
+});
