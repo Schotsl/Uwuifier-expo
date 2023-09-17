77a5813bccd10a22f7ed059be3c8c9e8d421bc01
Sjors van Holst <sjorsvanholst@gmail.com>
2023-09-17 22:16:27 +0200

    👔 Added Sentry

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 import Header from "./components/Header";
 import Editor from "./components/Editor";
@@ -8,8 +8,14 @@ import plausible from "./utils/plausible";
 
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { RootSiblingParent } from 'react-native-root-siblings';
+import { RootSiblingParent } from "react-native-root-siblings";
 import { ScrollView, StyleSheet, View } from "react-native";
+
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://acadd9a0860971ccb67c8b0b06552cc6@o4505897577414656.ingest.sentry.io/4505897580888064",
+});
 
 export default function App() {
   const [offset, setOffset] = useState(0);
@@ -24,7 +30,7 @@ export default function App() {
     <RootSiblingParent>
       <StatusBar style="light" />
 
-      <View style={[styles.body, {paddingTop: Constants.statusBarHeight}]}>
+      <View style={[styles.body, { paddingTop: Constants.statusBarHeight }]}>
         <ScrollView>
           <View style={styles.body__content}>
             <Header offset={offset} />
