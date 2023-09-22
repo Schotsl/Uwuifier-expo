80614193cb54ab587aacd13efca2ba8b4b1ec9fa
Sjors van Holst <sjorsvanholst@gmail.com>
2023-09-22 09:22:48 +0200

    🚸 Attempted to improve input

--- components/Editor/Input/index.tsx
+++ components/Editor/Input/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import EditorLabel from "../Label";
 
-import { View, TextInput, StyleSheet } from "react-native";
+import { useRef, useState } from "react";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 
 type EditorInputProps = {
   value: string;
@@ -15,20 +16,53 @@ export default function EditorInput({
   onFocus,
   onChange,
 }: EditorInputProps) {
+  const input = useRef(null);
+  const [focused, setFocused] = useState(false);
+
+  const handleWrapperPress = () => {
+    if (!input.current) {
+      return;
+    }
+
+    if (focused) {
+      input.current.blur();
+    } else {
+      input.current.focus();
+    }
+  };
+
+  const handleInputFocus = () => {
+    setFocused(true);
+
+    onFocus();
+  };
+
+  const handleInputBlur = () => {
+    setFocused(false);
+  };
+
   return (
     <View style={styles.input}>
       <EditorLabel color="#fff" background="#252525" label="Input" />
 
-      <TextInput
-        style={styles.input__wrapper__element}
-        value={value}
-        multiline={true}
-        onFocus={onFocus}
-        placeholder="Type something here..."
-        onChangeText={onChange}
-        textAlignVertical="top"
-        placeholderTextColor="grey"
-      />
+      <TouchableOpacity
+        style={styles.input__wrapper}
+        onPress={handleWrapperPress}
+        activeOpacity={1}
+      >
+        <TextInput
+          ref={input}
+          style={styles.input__wrapper__element}
+          value={value}
+          multiline={true}
+          onBlur={handleInputBlur}
+          onFocus={handleInputFocus}
+          onChangeText={onChange}
+          placeholder="Type something here..."
+          textAlignVertical="top"
+          placeholderTextColor="grey"
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -38,12 +72,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#252525",
   },
-  input__wrapper__element: {
+  input__wrapper: {
     flex: 1,
-    minHeight: 150,
-    color: "#ffffff",
-    fontSize: 17,
+    height: 150,
     marginVertical: 16,
     paddingHorizontal: 20,
+  },
+  input__wrapper__element: {
+    color: "#ffffff",
+    fontSize: 17,
   },
 });
